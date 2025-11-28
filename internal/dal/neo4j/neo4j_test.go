0f09ca36144e6daa1ad39d26ea9f4723343fa8cc
Silhouette <54932288+Silhouette-sophist@users.noreply.github.com>
2025-11-28 23:32:14 +0800

    [Feature]save query

--- internal/dal/neo4j/neo4j_test.go
+++ internal/dal/neo4j/neo4j_test.go
@@ -1,7 +1,27 @@
 package neo4j
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
 
 func TestInit(t *testing.T) {
-
+	ctx := context.Background()
+	connector, err := NewNeo4jConnector(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	first, err := connector.CreatePersonNode("one", 23)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("first node created: %v", first)
+	second, err := connector.CreatePersonNode("two", 32)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("second node created: %v", second)
+	if err = connector.CreateFriendship("one", "two", 2023); err != nil {
+		t.Fatal(err)
+	}
 }
